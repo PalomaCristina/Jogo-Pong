324f7cb485585476d3e295ae83f9c8245e04c3fb
Paloma Cristina <paloma1899@gmail.com>
2022-03-29 11:40:15 -0300

    incluindo a raquete do oponente

--- sketch.js
+++ sketch.js
@@ -2,53 +2,63 @@ var xBolinha = 300;
 var yBolinha = 200;
 var diametro = 25;
 
+// velocidade da bolinha
+
 var velocidadeXBolinha = 6;
 var velocidadeYBolinha = 6;
 var raio = diametro / 2;
 
+//raquete
 var xRaquete = 5;
 var yRaquete = 150;
 var larguraRaquete = 10;
 var alturaRaquete = 90;
+
+//raquete do oponente
+var xRaqueteOponente = 585;
+var yRaqueteOponente = 150;
 
 function setup() {
   createCanvas(600, 400);
 }
 function draw() {
   background(0);
-  xBolinha += velocidadeXBolinha; 
-  yBolinha += velocidadeYBolinha; 
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
   mostraBolinha();
   movimentaBolinha();
-  mostraRaquete();
+  mostraRaquete(xRaquete, yRaquete);
+  mostraRaquete(xRaqueteOponente, yRaqueteOponente);
   movimentaRaqueta();
   verificaColisaoRaquete();
-function mostraBolinha(){
-  circle(xBolinha,yBolinha,diametro);
-}
-function movimentaBolinha(){
-    if(xBolinha+raio  > width || xBolinha - raio < 0){
-    velocidadeXBolinha *= -1;
+  mostraRaqueteOponente();
+
+  function mostraBolinha() {
+    circle(xBolinha, yBolinha, diametro);
   }
-  if(yBolinha + raio > height || yBolinha - raio < 0){
-    velocidadeYBolinha *= -1;
+  function movimentaBolinha() {
+    if (xBolinha + raio > width || xBolinha - raio < 0) {
+      velocidadeXBolinha *= -1;
+    }
+    if (yBolinha + raio > height || yBolinha - raio < 0) {
+      velocidadeYBolinha *= -1;
     }
   }
 }
-function mostraRaquete(){
-    rect(xRaquete, yRaquete, larguraRaquete, alturaRaquete);
+function mostraRaquete(x, y) {
+  rect(x, y, larguraRaquete, alturaRaquete);
 }
 
-function movimentaRaqueta(){
-  if(keyIsDown(UP_ARROW)){
+function movimentaRaqueta() {
+  if (keyIsDown(UP_ARROW)) {
     yRaquete -= 10;
   }
-  if(keyIsDown(DOWN_ARROW)){
+  if (keyIsDown(DOWN_ARROW)) {
     yRaquete += 10;
   }
 }
-function verificaColisaoRaquete(){
-  if(xBolinha - raio < xRaquete + larguraRaquete && yBolinha - raio < yRaquete + alturaRaquete && yBolinha + raio > yRaquete){
+function verificaColisaoRaquete() {
+  if (xBolinha - raio < xRaquete + larguraRaquete && yBolinha - raio < yRaquete + alturaRaquete && yBolinha + raio > yRaquete) {
     velocidadeXBolinha *= -1;
   }
 }
